baa69a97b17055b3f77d353c5e52dfcffa3084f4
Seif Eldin Sameh <seiffsameh00@gmail.com>
2023-12-10 19:04:14 +0200

    picky properties

--- shared/Header.tsx
+++ shared/Header.tsx
@@ -7,12 +7,12 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
   NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
 } from "@nextui-org/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
