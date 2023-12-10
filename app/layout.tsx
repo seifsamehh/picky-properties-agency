e644a48c680adb0e9ff84572bed6c828479a92c5
Seif Eldin Sameh <seiffsameh00@gmail.com>
2023-12-10 20:23:49 +0200

    picky properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,9 @@ import { Roboto } from "next/font/google";
 import { Providers } from "./providers";
 import { Parallax } from "./parallax";
 import LoadingPage from "@/shared/LoadingPage";
+import Header from "@/shared/Header";
 import "./globals.css";
 import "../styles/palettes.scss";
-import Header from "@/shared/Header";
-import Head from "next/head";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -183,20 +182,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="preload"
-          as="font"
-          type="font/woff2"
-          href="/fonts/custom.woff2"
-        />
-        <link
-          rel="preload"
-          as="font"
-          type="font/ttf"
-          href="/fonts/custom.ttf"
-        />
-      </Head>
       <body className={roboto.className}>
         <LoadingPage />
         <Providers>
